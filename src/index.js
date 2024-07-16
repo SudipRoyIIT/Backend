7ff13e96efec2ec1e38175d96462fe3eef287f51
SudipRoyIIT <iitrcodalab@gmail.com>
2024-07-16 17:07:06 +0530

    Update index.js
    hii there

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.use("*",(req,res)=>{
 })
 
 
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3001;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
